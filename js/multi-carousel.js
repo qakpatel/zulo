185cfefa2aad7142611f7a287b2b11bb07a1459b
Karmvir Patel <karmvirpatel@QAIT.COM>
2019-11-12 15:08:18 +0530

    first commit

---
+++ js/multi-carousel.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
+
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
+
+    ResCarouselSize();
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        console.log(itemsDiv)
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+           console.log(btnParentSb)
+            if (bodyWidth >= 1200) {
+                console.log(incno)
+                console.log(itemsSplit)
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+                console.log(incno)
+                console.log(itemWidth,sampwidth)
+            }
+            else if (bodyWidth >= 992) {
+                console.log(incno)
+                console.log(itemsSplit)
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+                console.log(incno)
+                console.log(itemWidth,sampwidth)
+            }
+            else if (bodyWidth >= 768) {
+                console.log(incno)
+                console.log(itemsSplit)
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+                console.log(incno)
+                console.log(itemWidth,sampwidth)
+            }
+            else {
+                console.log(incno)
+                console.log(itemsSplit)
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+                console.log(incno)
+                console.log(itemWidth,sampwidth)
+            }
+            console.log(itemWidth,itemNumbers)
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+                console.log(itemWidth)
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        console.log()
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+
+});
---
+++ js/owl.js
@@ -0,0 +1,23 @@
+
+$('.owl-carousel').owlCarousel({
+    // loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    // autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 5
+      },
+      600: {
+        items: 5
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
